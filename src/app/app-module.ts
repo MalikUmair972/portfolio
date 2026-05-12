0042bca89c8cff989e93e02bbb857802d7c05958
Malik Umair <mu501362@gmail.com>
2026-05-12 23:53:05 +0500

    add components to AppModule declarations

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { Footer } from './components/footer/footer';
+import { ThreeBackground } from './components/three-background/three-background';
+import { Home } from './pages/home/home';
+import { ScrollTransition } from './components/scroll-transition/scroll-transition';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    Hero,
+    Footer,
+    ThreeBackground,
+    Home,
+    ScrollTransition
   ],
   imports: [
     BrowserModule,
