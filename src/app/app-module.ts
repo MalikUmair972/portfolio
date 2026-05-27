4ad7142559ca78edd5d962f1e435d50deee43bda
Malik Umair <mu501362@gmail.com>
2026-05-27 21:49:02 +0500

    refactor: reorganize imports and enhance code readability in app-module.ts

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,14 +1,25 @@
-import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  NgModule,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
-import { Header } from './components/header/header';
-import { Hero } from './components/hero/hero';
-import { Footer } from './components/footer/footer';
-import { ThreeBackground } from './components/three-background/three-background';
-import { Home } from './pages/home/home';
-import { ScrollTransition } from './components/scroll-transition/scroll-transition';
+import { AppRoutingModule }   from './app-routing-module';
+import { App }                from './app';
+import { Header }             from './components/header/header';
+import { Hero }               from './components/hero/hero';
+import { Footer }             from './components/footer/footer';
+import { Nav }                from './components/nav/nav';
+import { ThreeBackground }    from './components/three-background/three-background';
+import { ScrollTransition }   from './components/scroll-transition/scroll-transition';
+import { Services }           from './components/services/services';
+import { Portfolio }          from './components/portfolio/portfolio';
+import { Home }               from './pages/home/home';
+import { Contact }            from './pages/contact/contact';
+import { PortfolioDetail }    from './pages/portfolio-detail/portfolio-detail';
+import { PortfolioPage }      from './pages/portfolio-page/portfolio-page';
 
 @NgModule({
   declarations: [
@@ -16,18 +27,25 @@ import { ScrollTransition } from './components/scroll-transition/scroll-transiti
     Header,
     Hero,
     Footer,
+    Nav,
     ThreeBackground,
+    ScrollTransition,
+    Services,
+    Portfolio,
     Home,
-    ScrollTransition
+    Contact,
+    PortfolioDetail,
+    PortfolioPage,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection()
+    provideZonelessChangeDetection(),
   ],
-  bootstrap: [App]
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
